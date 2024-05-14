ddb50804a556d431867b573231380425638add90
edward <edward_gio@hotmail.com>
2024-05-14 17:43:26 -0500

    Fetch a pokemons, populate result in main

--- src/pages/Pokemons.tsx
+++ src/pages/Pokemons.tsx
@@ -1,40 +1,47 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import BulbasurPic from "../assets/bulbasaur.gif";
 import styles from "./pokemons.module.css";
+import { fetchPokemons } from "../api/fetchPokemons";
+import { Pokemon } from "../types/types";
 
 const Pokemons = () => {
   const [query, setQuery] = useState("");
+  const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+
+  useEffect(() => {
+    const fetchAllPokemons = async () => {
+      const allPokemons = await fetchPokemons();
+      console.log(allPokemons);
+      setPokemons(allPokemons);
+    };
+    fetchAllPokemons();
+  }, []);
+
   return (
     <>
       <Header query={query} setQuery={setQuery} />
       <main>
-        <nav>
-          <Link className={styles.listItem} to="/">
-            <img className={styles.listItemIcon} src={BulbasurPic} alt="bulbasaur" />
-            <div className={styles.listItemText}>
-              <span>Bulbasaur</span>
-              <span>001</span>
-            </div>
-          </Link>
-
-          <Link className={styles.listItem} to="/">
-            <img className={styles.listItemIcon} src={BulbasurPic} alt="bulbasaur" />
-            <div className={styles.listItemText}>
-              <span>Ivisur</span>
-              <span>002</span>
-            </div>
-          </Link>
-
-          <Link className={styles.listItem} to="/">
-            <img className={styles.listItemIcon} src={BulbasurPic} alt="bulbasaur" />
-            <div className={styles.listItemText}>
-              <span>Venosaur</span>
-              <span>003</span>
-            </div>
-          </Link>
+        <nav className={styles.nav}>
+          {pokemons?.slice(0, 151).map((pokemon) => (
+            <Link
+              key={pokemon.id}
+              className={styles.listItem}
+              to={`/pokemons/${pokemon.name.toLocaleLowerCase}`}
+            >
+              <img
+                className={styles.listItemIcon}
+                src={pokemon.imgSrc}
+                alt={pokemon.name}
+              />
+              <div className={styles.listItemText}>
+                <span>{pokemon.name}</span>
+                <span>{pokemon.id}</span>
+              </div>
+            </Link>
+          ))}
         </nav>
       </main>
       <Footer />
